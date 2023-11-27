52edd94b0962bf958c51b814957462de60affc01
David Doria <daviddoria@Davids-M1-MacBook-Pro.local>
2023-11-27 08:36:49 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
 
-function App() {
+function loginReducer(state, action) {
+  switch (action.type) {
+    case "field": {
+      return {
+        ...state,
+        [action.fieldName]: action.payload,
+      };
+    }
+    case "login": {
+      return {
+        ...state,
+        error: "",
+        isLoading: true,
+      };
+    }
+    case "success": {
+      return {
+        ...state,
+        isLoggedIn: true,
+        isLoading: false,
+      };
+    }
+    case "error": {
+      return {
+        ...state,
+        error: "Incorrect username or password!",
+        isLoggedIn: false,
+        isLoading: false,
+        username: "",
+        password: "",
+      };
+    }
+    case "logOut": {
+      return {
+        ...state,
+        isLoggedIn: false,
+      };
+    }
+    default:
+      return state;
+  }
+}
+const initialState = {
+  username: "",
+  password: "",
+  isLoading: false,
+  error: "",
+  isLoggedIn: false,
+};
+export default function LoginUseReducer() {
+  const [state, dispatch] = useReducer(loginReducer, initialState);
+  const { username, password, isLoading, error, isLoggedIn } = state;
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "login" });
+    try {
+      await new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (username === "ejiro" && password === "password") {
+            resolve();
+          } else {
+            reject();
+          }
+        }, 1000);
+      });
+      dispatch({ type: "success" });
+    } catch (error) {
+      dispatch({ type: "error" });
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login-container">
+        {isLoggedIn ? (
+          <>
+            <h1>Welcome {username}!</h1>
+            <button onClick={() => dispatch({ type: "logOut" })}>
+              Log Out
+            </button>
+          </>
+        ) : (
+          <form className="form" onSubmit={onSubmit}>
+            {error && <p className="error">{error}</p>}
+            <p>Please Login!</p>
+            <input
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) =>
+                dispatch({
+                  type: "field",
+                  fieldName: "username",
+                  payload: e.currentTarget.value,
+                })
+              }
+            />
+            <input
+              type="password"
+              placeholder="password"
+              autoComplete="new-password"
+              value={password}
+              onChange={(e) =>
+                dispatch({
+                  type: "field",
+                  fieldName: "password",
+                  payload: e.currentTarget.value,
+                })
+              }
+            />
+            <button className="submit" type="submit" disabled={isLoading}>
+              {isLoading ? "Logging in..." : "Log In"}
+            </button>
+          </form>
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
