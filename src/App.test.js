7c24add772fd2b30f6d376ab29bae4257186c5ff
David Doria <daviddoria@Davids-M1-MacBook-Pro.local>
2023-11-28 13:19:10 -0800

    Test failing test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Login/i);
+  const linkElement = screen.getByText(/Logixn/i);
   expect(linkElement).toBeInTheDocument();
 });
